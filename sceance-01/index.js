baa3f8745857cfa7f5182951bf902257567d744b
elfakirimariam <156078645+elfakirimariam@users.noreply.github.com>
2024-01-30 09:53:12 +0100

    test

---
+++ sceance-01/index.js
@@ -0,0 +1,35 @@
+ console.log("hello javascript!!!")  //print//
+ function greet(nom) {
+    console.log("hello!" +nom)
+ }
+
+ greet("elfakiiri")
+
+
+ function somme(nombre1,nombre2){
+    return nombre1+nombre2
+ }
+console.log(somme(13,90))
+
+//declaration des variables
+const PI = 3.14
+function inscrire(){
+    let nom;
+    let prenom;
+    let age;
+    nom="ELFAKIRI"
+    prenom="MARIAM";
+    age=21
+    if (age < 22){
+        var statut;
+        statut ="stagiaire"
+    }
+    console.log(nom)
+    console.log(statut)
+
+
+}
+let texte1 = "OFPPT";
+texte1 += "";
+let texte2 ="en force";
+let texte3= texte1+texte2;
---
+++ sceance-02/index.js
@@ -0,0 +1,12 @@
+let a=5;
+let b="5";
+if(a === b){
+    //console.log("ok!!!")  
+}
+
+
+let statut="etudiant";
+let age=20;
+if (statut=="etudiant" || age>20){
+    console.log("ok!!!")
+}
